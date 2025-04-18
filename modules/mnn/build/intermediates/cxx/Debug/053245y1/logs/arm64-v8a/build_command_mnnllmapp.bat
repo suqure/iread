0a31b9e56ebcd42f3590182075d5179cff072f98
suqure <167196623+suqure@users.noreply.github.com>
2025-04-18 11:50:20 +0800

    移除无效代码
    移除无效代码

--- modules/mnn/build/intermediates/cxx/Debug/053245y1/logs/arm64-v8a/build_command_mnnllmapp.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"D:\\Program Files\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  -C ^
-  "D:\\Android\\voiceread\\modules\\mnn\\.cxx\\Debug\\053245y1\\arm64-v8a" ^
-  mnnllmapp
--- modules/mnn/build/intermediates/cxx/RelWithDebInfo/3r5a5h43/logs/x86_64/build_command_mnnllmapp.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"D:\\Program Files\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  -C ^
-  "D:\\Android\\voiceread\\modules\\mnn\\.cxx\\RelWithDebInfo\\3r5a5h43\\x86_64" ^
-  mnnllmapp
